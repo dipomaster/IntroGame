d24db5cdc8c95b65eb7b129fb2f8256bebfb5f18
Dipo Master <mstrdipo@gmail.com>
2021-10-08 10:57:48 +0100

    scene

---
+++ UnityProj/Assets/PlayerInput.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector2 moveValue;
+    public float speed;
+
+    
+}
